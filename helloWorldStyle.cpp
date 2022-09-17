d6f85f2ae7c1771acb9fe960d77409312e3f7d39
Kaitlin Green <kaitlin.green@stu.ocsb.ca>
2022-09-17 03:57:02 +0000

    CPP scares me but its actually pretty fun at the moment. CPP, you are growing on me. I have finished the code for my updated CPP with style.

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Katie G All rights reserved.
+//.
+// Created by: Katie
+// Date: September 16th, 2022
+// This program displays "Hello people of the world! This has proper style. My friend still has a parasocial relationship with Hank Anderson." with proper style, in C++ now!
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello people of the world! This has proper style. My friend still has a parasocial relationship with Hank Anderson." << std::endl;
+    std::cout << "I tried to copy paste the script of an entire Star Trek episode into my code but it didn't work. That made me sad." << std::endl;
+}
